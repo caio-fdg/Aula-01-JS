9617b0e8bf7ecfdba2ecd2886fe885839fc04e69
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-21 11:51:06 -0300

    Variáveis, declarações e tipos

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// Declaração
+// variavel sempre em letra minuscula
+var nome = "Caio"
+console.log(typeof nome)
+
+let idade = 33
+console.log(typeof idade)
+
+const dev = true
+console.log(typeof dev)
+
+let dados = ["um", "dois"]
+console.log(typeof dados)
